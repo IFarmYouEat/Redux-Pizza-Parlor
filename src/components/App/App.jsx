e7a34ad6149bae1ebbaf309bfd908620455fb1f4
Brock Nelson <Bmnelson87@gmail.com>
2023-11-09 11:28:28 -0600

    setup for group project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { useSelector, useDispatch} from 'react-redux';
+import { HashRouter as Router, Route, Link} from 'react-router-dom';
+
 
 function App() {
+  // Display Pizzas
+
+
+  // Customer Information Page
+
+
+  // Checkout Page
+
+
+  // End Checkout Page
 
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
+
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
-  
+      <Router>
+      {/* Display Pizzas */}
+
+      {/* Customer Information Page */}
+
+      {/* Checkout Page */}
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { Provider } from 'react-redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import logger from 'redux-logger';
+
 import './index.css';
 import App from './components/App/App';
+
+// Ben
+
+// Brock
+
+// Andy
+
+
+
+
+const reduxStore = createStore(
+    combineReducers({
+        // Ben
+
+        // Brock
+
+        // Andy
+
+    }),
+    applyMiddleware(logger)
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={reduxStore}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
